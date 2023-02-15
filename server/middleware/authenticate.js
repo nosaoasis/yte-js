99b0d79a66ec7fe426f6a6775fd3c225622874ae
Nosa Agho <nosaoasis@gmail.com>
2023-02-15 14:29:40 +0100

    update

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,63 +1,32 @@
-const jwt = require("jsonwebtoken")
+const jwt = require("jsonwebtoken");
 
 const adminUserAuthenticateMiddleware = async (req, res, next) => {
-  console.log("aaa")
+  console.log("aaa");
   const authHeader = req.headers.authorization;
-  console.log(authHeader)
-  console.log("bbb")
-  
+  console.log(authHeader);
+  console.log("bbb");
+
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    console.log("Auth header error");
     res.status(401).json({ msg: "Error", response: "Unauthorized" });
-    return
+    return;
   }
-  
+
   const token = authHeader.split(" ")[1];
-  
+
   try {
-    const decodeToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    console.log("ccc")
-    console.log("authenticate token value is ", decodeToken)
-    req.token = decodeToken
-    next()
+    const decodeToken = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("ccc");
+    console.log("authenticate token value is ", decodeToken);
+    req.token = decodeToken;
+    next();
   } catch (error) {
     res.status(401).json({ msg: "Error", response: "Unauthorized" });
   }
-}
+};
 
-const authLoginRegisterPage = (req, res) => {
-  console.log("authenticating page access....")
-  
-  const authHeader = req.headers.authorization;
-  console.log("authheader values is ", authHeader)
-  
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    res.status(401).json({ msg: "Error", response: "Unauthorized" });
-    return
-  }
-  
-  const token = authHeader.split(" ")[1];
-  console.log("====",token)
-  
-  if (token === "null") {
-    console.log("dfknmfkmklf")
-    return res.status(401).json({ msg: "Error", response: "Unauthorized" });
-  }
 
-  try {
-    const decodeToken = jwt.verify(token, process.env.JWT_SECRET_KEY);
-    console.log("ccc")
-    console.log("authenticate token value is ", decodeToken)
-    res.status(200).json({msg: "Success", response: decodeToken})
-
-    // next()
-  } catch (error) {
-    res.status(401).json({ msg: "Error", response: "Unauthorized" });
-  }
-  // next()
-  // return res.status(401).json({ msg: "Success", response: "Authorized" });
-}
 
 module.exports = {
-  adminUserAuthenticateMiddleware,
-  authLoginRegisterPage
-}
+  adminUserAuthenticateMiddleware
+};
